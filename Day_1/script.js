dfffce79e35eb0cf9628bb21290d5755880b2590
nehasharmacode123 <nehasharma962595@gmail.com>
2025-11-04 18:28:15 +0530

    first commit

---
+++ Day_1/script.js
@@ -0,0 +1,195 @@
+// 4 november 2025
+
+// console.log("Hello world!!!!!");
+
+// Variable
+
+// let favoriteNumberTwo = 3;
+
+// console.log(favoriteNumberTwo);
+
+// favoriteNumberTwo = 4
+// console.log(favoriteNumberTwo);
+
+// let a = 5;
+
+// let b= 2;
+
+// let c= 3;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// a = 3;
+// console.log(a);
+
+// let d = a;
+
+// console.log(d);
+// console.log(a);
+
+// let int = 2; //int
+// let float = 2.3
+// console.log(typeof int);
+// console.log(typeof float);
+// console.log(float + int);
+
+// console.log((1+2)*3);
+
+// let a = 1.2;
+
+// let b = 2.2;
+
+// let c = 3;
+
+// console.log(a+b);
+// console.log(a*b);
+
+// let a = "Hello world"
+
+// a = 'John say to "Hi"'
+// a = "John say to \"Hi\""
+
+// console.log(a);
+
+// let name = "Kyle";
+
+// console.log("Hello my name is", name);
+// console.log("Hello my name is " + name);
+
+// let a = "true"; //string
+// a = true;
+// let b = false;
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(a + b); //1
+// console.log(a && b); //false
+// console.log(a || b); //true
+
+// console.log((false && false) || true);
+// console.log(false && (false || true)); //false
+
+// let happy = true;
+// let fun = false;
+
+// console.log(!happy);
+// console.log(!!happy);
+
+
+// console.log(happy && fun);
+
+// let a = undefined;
+
+// console.log(a);
+// console.log(typeof a);
+
+// let b = null;
+
+// console.log(b);
+// console.log(typeof b);
+
+// let a = 1;
+// let b = 2;
+
+
+// console.log(!a == b);
+// console.log(a != b);
+// console.log(a < b); //true
+// console.log(a > b); //true
+// console.log(a >= b); //true
+// console.log(a <= b); //true
+
+
+// let a = 'a'; //65
+// let b = 'b'; //66
+// let b = 'a';
+
+// console.log(a != b);
+// console.log(a < b);
+// console.log(a > b);
+
+// let a = false;
+// let b = false;
+
+// console.log(a == b);
+// console.log(a != b);
+
+// let a = null;
+// let b = undefined;
+
+// console.log(a == b); //true
+// console.log(a != b); //false
+
+// let a = 1;
+
+// console.log(a);
+
+// let b = 2;
+
+// console.log(b);
+
+// let zzzzz = 100000000;
+
+// console.log(zzzzz);
+
+// function sayHi(){
+//     console.log("Hi");
+//     console.log("Bye");
+    
+// }
+
+// sayHi()
+
+// function printName() {
+//     console.log("Neha Sharma");
+    
+// }
+
+// printName()
+
+
+// function sum(a,b){
+//     console.log(a+b);
+    
+// }
+
+// let x = 1;
+// let y = 2;
+
+// sum(2, 3)
+// sum(x, y)
+
+// function onePersonPrintName(name, age){
+//     console.log(name);
+//     console.log(age);
+    
+// }
+
+// onePersonPrintName("Neha Sharma")
+// onePersonPrintName() //undefined
+
+// function sum(a, b){
+//     return a + b
+// }
+
+// let s = sum(1, 2);
+// let sum2 = sum(2, s)
+// console.log(sum2);
+
+function name(name){
+    return "hello" + name
+}
+
+let s = name("neha");
+console.log(s);
+
+
+
+
+// 13 variable comparison (again practice)
+//  14 garbage collection (again practice )
+
+
+
