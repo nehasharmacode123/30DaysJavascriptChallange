c6168522065359643d9a80054c82153b1be1c31f
nehasharmacode123 <nehasharma962595@gmail.com>
2025-11-06 22:53:54 +0530

    day_3

---
+++ Day_3/script.js
@@ -0,0 +1,60 @@
+// function doStuff(a,b, name){
+//     print(sum(a,b));
+//     print(sayHi(name))
+// }
+
+// function print(variable){
+//     console.log(variable);
+    
+// }
+
+// function sum(a,b){
+//     return a + b;
+// }
+
+// function sayHi(name){
+//     return "Hi " + name
+// }
+
+// doStuff(1,2, "neha")
+
+// hoisting
+
+// function sum(a,b){
+//     return a + b;
+// }
+
+// console.log(sum(1,2));
+// console.log(sum(a,2));
+
+// let a = 1;
+
+// console.log(sum(1,2));
+
+// function sum(a,b){
+//     return a + b
+// }
+
+// function sum(a,b){
+//     return a + b;
+// }
+
+// console.log(sumArrow(1,2));
+
+// let a = 1;
+// let sumArrow = (a,b) => {
+//     return a + b;
+// }
+
+// # scoping
+
+function sayHi(name){
+    let result = "Hi " + name;
+    console.log(result);
+    
+}
+
+let name = "anjali"
+
+sayHi(name)
+
